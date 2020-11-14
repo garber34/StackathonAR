1b06d790b7f4f9f30ceb95e73f1b88f04010d42b
Jeff <garber34@gmail.com>
2020-11-14 01:15:29 -0500

    fixing tracking index

--- markerHandler.js
+++ markerHandler.js
@@ -61,8 +61,13 @@ const assets = {
     scale: ".015 .05 .015",
     position: "0 0 0",
   },
- };
-
+  avatar: {
+    url: "/Models/avatar/scene.gltf",
+    rotation: "0 -90 -90",
+    scale: "1 1 1",
+    position: "0 0 0",
+  }
+};
 
 const markers = {
   fire: "/markers/firetext.patt",
@@ -95,36 +100,34 @@ AFRAME.registerComponent("setup", {
       marker.setAttribute("url", markers[key]);
       marker.setAttribute("register-events", true);
 
-      const entity = document.createElement("a-gltf-model");
-      entity.setAttribute("animation-mixer", "loop: repeat");
-      setEntityAttributes(entity, key);
+      //create and append all models relevant to marker
+      for (asset in assets) {
+        if (asset.includes(key) || asset==="avatar") {
+          const entity = document.createElement("a-gltf-model");
+          entity.setAttribute("id", asset);
+          entity.setAttribute("animation-mixer", "loop: repeat");
+          entity.setAttribute("visible", asset === key ? true : false);
+          setEntityAttributes(entity, asset);
 
-      marker.appendChild(entity);
+          marker.appendChild(entity);
+        }
+      }
 
       scene.appendChild(marker);
     }
-
-    console.log("SCENE", scene);
   },
 });
 
 AFRAME.registerComponent("register-events", {
   init: function () {
     const marker = this.el;
-    console.log("REGISTER EVENTS ON ", marker.id);
 
     marker.addEventListener("markerFound", () => {
-      const markerId = marker.id;
-      console.log("Marker FOUND: ", markerId);
-      console.log("Adding to list of found markers");
-      console.log("FOUND MARKER'S ENTITY", marker.childNodes[0]);
       currentMarkers.push(marker);
-      console.log("CURRENT MARKERS ARE", currentMarkers);
       if (currentMarkers.length > 1) combineModels(currentMarkers);
     });
     marker.addEventListener("markerLost", () => {
       resetModel(marker);
-      console.log("Current Markers are", currentMarkers);
     });
   },
 });
@@ -132,48 +135,52 @@ AFRAME.registerComponent("register-events", {
 function resetModel(marker) {
   // reset model to factory default
   if (marker.id !== marker.childNodes[0].getAttribute("src").slice(1))
-    clearAndCreate(marker, marker.id);
+    setVisbility(marker, marker.id);
 
   //remove the reset marker from the list of seen marker
   currentMarkers = currentMarkers.filter((el) => el.id !== marker.id);
 
   //reset the other marker left in the list to factory default, since there is no other marker to interact with
   if (currentMarkers.length > 0) {
-    const otherMarker = currentMarkers[0];
-    clearAndCreate(otherMarker, otherMarker.id);
+   currentMarkers.forEach(otherMarker =>{
+    setVisbility(otherMarker, otherMarker.id);
+   })
   }
 }
 
 function combineModels(currentMarkers) {
-  console.log("COMBINING", currentMarkers[0]);
-  console.log("WITH", currentMarkers[1]);
+  if(currentMarkers.length===2){
   const ids = [currentMarkers[0].id, currentMarkers[1].id];
   ids.sort();
   const combinedAssetName = ids[0] + ids[1];
-  console.log("COMBINED ASSET NAME", combinedAssetName);
 
-  currentMarkers.forEach((marker) => {
-    clearAndCreate(marker, combinedAssetName);
-  });
+
+    setVisbility(currentMarkers[0], combinedAssetName);
+    setVisbility(currentMarkers[1], "none");
+
+  }
+  if(currentMarkers.length===3){
+    currentMarkers.forEach(marker => {
+      setVisbility(marker, marker.id)
+    })
+  }
+  if (currentMarkers.length===4){
+    setVisbility(currentMarkers[0], "avatar")
+    setVisbility(currentMarkers[1], "none");
+    setVisbility(currentMarkers[2], "none");
+    setVisbility(currentMarkers[3], "none");
+  }
 }
 
 function setEntityAttributes(entity, assetKey) {
-  console.log("SETTING ATTIBUTES OF", entity);
-  console.log("TO MODEL", assetKey);
-
   entity.setAttribute("src", `#${assetKey}`);
   entity.setAttribute("scale", assets[assetKey].scale);
   entity.setAttribute("rotation", assets[assetKey].rotation);
   entity.setAttribute("position", assets[assetKey].position);
 }
 
-function clearAndCreate(marker, assetKey) {
-  console.log("========CLEARING=======");
-  marker.removeChild(marker.firstChild);
-
-  const entity = document.createElement("a-gltf-model");
-  entity.setAttribute("animation-mixer", "loop: repeat");
-  setEntityAttributes(entity, assetKey);
-
-  marker.appendChild(entity);
+function setVisbility(marker, assetKey) {
+  marker.childNodes.forEach((model) => {
+    model.setAttribute("visible", model.id === assetKey ? true : false);
+  });
 }
